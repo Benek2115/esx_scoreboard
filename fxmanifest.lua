f6b873e79c437a4a69e5fbd845c888599a4a7a47
Benek2115 <99657184+Benek2115@users.noreply.github.com>
2023-03-13 22:31:14 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+
+game 'gta5'
+lua54 'yes'
+shared_script '@BestSide-Handler/shared/shared.lua'
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'html/scoreboard.html'
+
+files {
+	'html/scoreboard.html',
+	'html/style.css',
+	'html/reset.css',
+	'html/listener.js',
+	'html/img.png'
+}
+
+server_exports {
+	'MisiaczekPlayers',
+	'CounterPlayers'
+}
+
+client_script '@BestSide/BasicAC.lua'
